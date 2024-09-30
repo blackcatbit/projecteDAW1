8ce6190c3915302d5b1c41f95749ede46772ff5f
blackcatbit <lorenapardo@icloud.com>
2024-09-30 08:35:13 +0200

    he subido carpeta de codi probando commit

---
+++ codi/html_js/hola.js
@@ -0,0 +1,2 @@
+let a = "Hola";
+alert(a); //Cuando se abre el html en el navegador llama a este script y aparece el mensaje de Hola en popup.
---
+++ codi/php/holamon.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>PHP hello world</title>
+    </head>
+    <body>
+        <?php
+        Print "Hello World!";
+        ?>
+
+        <?php
+        Echo "Hello, World!";
+        ?>
+    </body>
